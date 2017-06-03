5b368efed61149bc3b2e1b642049dfbebe041a4a
Alexander Taxter <Sharquo@gmail.com>
2017-06-03 06:25:15 +0100

    Finished chapter 5.

---
+++ Assets/Scripts/MoveForward.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour {
+
+	public float speed = 0.3f;
+	private Rigidbody2D velo;
+	
+	private void Awake()
+	{
+		velo = GetComponent<Rigidbody2D>();
+	}
+	void Update () {
+		velo.velocity = new Vector2 (this.transform.localScale.x, 0) * speed;		
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 200;
+	public float maxSpeed = 5;
+	int moving = 0;
+	
+	private Rigidbody2D rigi;
+	private void Awake ()
+	{
+		rigi = GetComponent<Rigidbody2D>();
+	}
+
+	void Update () {
+		if (Input.GetKey("right"))
+		{
+			moving = 1;
+		}
+		else if (Input.GetKey("left"))
+		{
+			moving = -1;
+		}
+		else
+		{
+			moving =0;
+		}
+
+		if (moving != 0)
+		{
+			var velocityX = System.Math.Abs(rigi.velocity.x);
+
+			if (velocityX < 0.5)
+			{
+				rigi.AddForce(new Vector2(moving, 0) * speed);
+
+				if (this.transform.localScale.x != moving)
+				this.transform.localScale = new Vector3(moving, 1, 1);
+				{
+					if (velocityX > maxSpeed)
+					rigi.velocity = new Vector2(maxSpeed * moving, 0);
+				}
+			}
+		}
+	}
+}
