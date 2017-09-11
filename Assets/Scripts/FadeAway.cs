c4491ad239becf80713e8fd7d0ff575b4a154d18
Alexander Taxter <Sharquo@gmail.com>
2017-09-11 10:15:12 +0100

    I have no idea why these changes are here.

---
+++ Assets/Scripts/FadeAway.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeAway : MonoBehaviour {
+
+	public float delay = 2.0f;
+	
+	void Start () {
+		StartCoroutine(FadeTo(0.0f, 1.0f));
+	}
+
+    IEnumerator FadeTo(float aValue, float aTime)
+    {
+        yield return new WaitForSeconds(delay);
+
+		float alpha = transform.GetComponent<Renderer>().material.color.a;
+		for (float t = 0.0f; t <= 1.0f; t += Time.deltaTime / aTime)
+		{
+			Color newColor = new Color(1, 1, 1, Mathf.Lerp(alpha, aValue,t));
+			transform.GetComponent<Renderer>().material.color = newColor;
+
+			if(newColor.a <= 0.05)
+			{
+				Destroy(gameObject);
+			}
+			yield return null;
+		}
+    }
+
+    void Update () {
+		
+	}
+}
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,6 +6,8 @@ public class Health : MonoBehaviour {
 
 	public int maxHealth = 10;
 	public int health = 10;
+	public GameObject deathInstance = null;
+	public Vector2 deathInstanceOffset = new Vector2(0,0);
 	void Start () {
 		health = maxHealth;
 	}
@@ -24,6 +26,13 @@ public class Health : MonoBehaviour {
 	}
 
 	void OnKill() {
+		if (deathInstance)
+		{
+			var pos = gameObject.transform.position;
+			GameObject clone = Instantiate (deathInstance, new 
+			Vector3(pos.x + deathInstanceOffset.x, pos.y + deathInstanceOffset.y, 
+			pos.z), Quaternion.identity) as GameObject;
+		}
 		Destroy(gameObject);
 	}
 }
